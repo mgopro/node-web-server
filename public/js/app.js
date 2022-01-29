6ba54bdb7cc3529b96913e5c70a4f8970e913aa5
ISG9972 <sowhat.9211@gmail.com>
2022-01-28 23:57:38 -0500

    comment console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 
-console.log('client side javascript has been loaded')
+// console.log('client side javascript has been loaded')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data) => {
